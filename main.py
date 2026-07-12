47c699770cac0a08d7314696f3c18bfa7493bde2
dgsiilvaa020-cmyk <dg.siilvaa020@gmail.com>
2026-07-11 21:06:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1769,26 +1769,17 @@ async def receber_figurinha(message: Message):
                 + pacote["sinopse"]
             )
 
-        sinopse_completa = None
-
-        # Telegram aceita no máximo 1024 caracteres na legenda
-        if len(caption) > 1500:
-
-            sinopse = pacote.get("sinopse", "")
-
-            if sinopse:
-
-                caption = caption.replace(
-                    "\n\n📖 SINOPSE:\n\n" + sinopse,
-                    ""
-                )
-
-                sinopse_completa = sinopse
-
         await bot.send_photo(
             chat_id=GRUPO_ACERVO,
             photo=pacote["capa"],
             caption=caption
+        )
+
+        if sinopse_completa:
+
+        await bot.send_message(
+            chat_id=GRUPO_ACERVO,
+            text=f"📖 SINOPSE\n\n{sinopse_completa}"
         )
 
         for arquivo_id in pacote["arquivos"]:
